b988953b57a43252b9b543e4a8692c1627f73d09
Arkanafarel <arkanafarel811@gmail.com>
2026-01-31 20:28:02 +0700

    rename css file

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
-const sections = document.querySelectorAll("section");
-const navLinks = document.querySelectorAll("nav a");
+const btn = document.getElementById("toggle-dark");
 
-const observer = new IntersectionObserver(
-    entries => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                navLinks.forEach(link => {
-                    link.classList.remove("active");
-                    if (link.getAttribute("href") === `#${entry.target.id}`) {
-                        link.classList.add("active");
-                    }
-                });
-            }
-        });
-    },
-    { threshold: 0.6 }
-);
-
-sections.forEach(section => observer.observe(section));
+btn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+});
