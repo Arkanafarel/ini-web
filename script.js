74456b647943b4fc91541abe021832936a73e2a1
Arkanafarel <arkanafarel811@gmail.com>
2026-01-31 21:01:23 +0700

    card style and scroll animation

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ const btn = document.getElementById("toggle-dark");
 btn.addEventListener("click", () => {
     document.body.classList.toggle("dark");
 });
+
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+    reveals.forEach(el => {
+        const windowHeight = window.innerHeight;
+        const elementTop = el.getBoundingClientRect().top;
+
+        if (elementTop < windowHeight - 100) {
+            el.classList.add("active");
+        }
+    });
+}
+
+window.addEventListener("scroll", revealOnScroll);
