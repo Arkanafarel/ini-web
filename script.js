fda6feb1b7fd6a0c00870a2efe103c87c6c7663f
Arkanafarel <arkanafarel811@gmail.com>
2026-02-01 11:03:33 +0700

    change style header and add type effect

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const text = "Arkan Afarel";
+    const typingSpeed =150;
+    const deletingSpeed = 100;
+    const delayAfterTyping = 3000;
+
+    let i = 0;
+    let isDeleting = false;
+    const target = document.getElementById("typing");
+
+    function typeLoop() {
+        if (!isDeleting) {
+            // NGETIK
+            target.innerHTML = text.substring(0, i + 1);
+            i++;
+            if (i === text.length) {
+                setTimeout(() => isDeleting = true, delayAfterTyping);
+            }
+        } else {
+            // MENGHAPUS
+            target.innerHTML = text.substring(0, i - 1);
+            i--;
+            if (i === 0) {
+                isDeleting = false;
+            }
+        }
+
+        setTimeout(typeLoop, isDeleting ? deletingSpeed : typingSpeed);
+    }
+
+    typeLoop();
+});
+
+
+
 const btn = document.getElementById("toggle-dark");
 
 btn.addEventListener("click", () => {
@@ -18,3 +53,4 @@ function revealOnScroll() {
 }
 
 window.addEventListener("scroll", revealOnScroll);
+
