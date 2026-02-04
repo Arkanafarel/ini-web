1721b97d3ed2980171b61cbb2b9a5d7770b2c839
Arkanafarel <arkanafarel811@gmail.com>
2026-02-04 11:44:19 +0700

    add hamburger menu

--- script.js
+++ script.js
@@ -100,3 +100,15 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.getElementById("hamburger");
+    const navLinks = document.querySelector(".nav-links");
+  
+    if (!hamburger || !navLinks) return;
+  
+    hamburger.addEventListener("click", () => {
+      navLinks.classList.toggle("active");
+    });
+  });
+  
